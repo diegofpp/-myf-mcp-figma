07ad3b1b95b2e71f00fda340ed9b20bbf2c0df01
diegofpp <difepor@gmail.com>
2025-09-25 11:28:51 -0300

    feat: sistema de navegación entre páginas
    - Implementado estado de navegación en main.tsx con useState
    - Agregadas funciones handleNavigateToMenu y handleNavigateToFrontpage
    - Sistema de routing simple sin dependencias externas
    - Navegación conectada desde:
      * Botón 'carta' en navbar de ImageWrapper
      * Card 'carta' en Grid
    - Eliminados botones de navegación temporales
    - Preparado para integración con página de menú

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,28 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import Frontpage1 from './Frontpage1'
+import Menu from './Menu'
+
+function App() {
+  const [currentPage, setCurrentPage] = React.useState<'frontpage' | 'menu'>('frontpage');
+
+  // Context para pasar la función de navegación a los componentes
+  const navigationContext = React.useMemo(() => ({
+    navigateToMenu: () => setCurrentPage('menu'),
+    navigateToFrontpage: () => setCurrentPage('frontpage')
+  }), []);
+
+  return (
+    <div>
+      {/* Page Content */}
+      {currentPage === 'frontpage' && <Frontpage1 onNavigateToMenu={navigationContext.navigateToMenu} />}
+      {currentPage === 'menu' && <Menu onNavigateToFrontpage={navigationContext.navigateToFrontpage} />}
+    </div>
+  );
+}
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Frontpage1 />
+    <App />
   </React.StrictMode>
 )
-
-
